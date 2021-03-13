9af56858f4ae1c948dc058e5b397fe19a0e7ebd5
Blake Michalzik <github+bmichalzik@gmail.com>
2021-03-12 19:00:12 -0800

    Added Commented Code to access webview for fragment in future.

--- app/src/main/java/com/bcit/streamerdex/StreamerdexMainActivity.java
+++ app/src/main/java/com/bcit/streamerdex/StreamerdexMainActivity.java
@@ -3,6 +3,9 @@ package com.bcit.streamerdex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class StreamerdexMainActivity extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class StreamerdexMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_streamerdex_main);
-        
+
+//        BM - Code for getting stream
+//        String url = "https://iblake.netlify.app/streamerdex/codemiko";
+//
+//        WebView mWebView = (WebView) findViewById(R.id.stream_view);
+//        mWebView.setInitialScale(1);
+//        WebSettings webSettings = mWebView.getSettings();
+//        webSettings.setJavaScriptEnabled(true);
+//        webSettings.setUseWideViewPort(true);
+//        webSettings.setLoadWithOverviewMode(true);
+//        mWebView.loadUrl(url);
+
     }
 }
