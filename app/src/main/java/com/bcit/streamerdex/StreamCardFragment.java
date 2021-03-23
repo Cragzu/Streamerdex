efffda4c646abf0778aa6155ef5af257f4c9082e
Chloe (Desktop) <cragzu@gmail.com>
2021-03-22 19:00:53 -0700

    placed stream web view inside stream card fragment

--- app/src/main/java/com/bcit/streamerdex/StreamCardFragment.java
+++ app/src/main/java/com/bcit/streamerdex/StreamCardFragment.java
@@ -3,6 +3,7 @@ package com.bcit.streamerdex;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 import androidx.annotation.NonNull;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class StreamCardFragment extends Fragment {
+
     private WebView streamView;
 
     public StreamCardFragment()
@@ -28,6 +30,16 @@ public class StreamCardFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.d("Lifecycle fragment", "onViewCreated");
         streamView = view.findViewById(R.id.stream_view);
+
+        String url = "https://iblake.netlify.app/streamerdex/codemiko";
+
+        streamView.setInitialScale(1);
+        WebSettings webSettings = streamView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        streamView.loadUrl(url);
+
     }
 
     public void setStreamView(String videoLink) {
